93b347ea184923eda913d97c50152b9c43098cec
Patrick McMahon <pmcmahon@carnegielearning.com>
2025-02-21 12:29:05 -0500

    adding new class to index for export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+export { AdjustedTime } from "./adjustedTime";
 export {
 	CaliperDuration,
 	CaliperSettings,
 	CaliperTimestamp,
 	default as Caliper,
 	URN,
-} from './caliper';
-export * from './clients/httpClient';
-export { Config } from './config/config';
-export { Envelope, EnvelopeOptions } from './envelope';
-export * from './models';
-export * from './sensor';
+} from "./caliper";
+export * from "./clients/httpClient";
+export { Config } from "./config/config";
+export { Envelope, EnvelopeOptions } from "./envelope";
+export * from "./models";
+export * from "./sensor";
